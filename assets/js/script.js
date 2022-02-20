03de6b9dd5091ce77f6368fedaa25008c00a4b53
WinnieCWu <winniechn7@gmail.com>
2022-02-20 08:35:28 -0800

    updated quiz question and answer choice elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
+//global variables for DOM elements
 var startQuiz = document.querySelector('#start-quiz-button')
 var timerEl = document.querySelector('#timer')
 var start = document.querySelector('#start')
-
 var timer = 60;
-
+var quizBox = document.querySelector(".quiz-box")
+var quizQuestion
+var quizAnswerOne
+var quizAnswerTwo
+var quizAnswerThree
+var quizAnswerFour
+var userScore = 0
 /*
 onClick of Start Button, lead the function of arrayOfQuestions for jQuery
 $("#start-quiz-button").click(function() {
@@ -11,152 +17,139 @@ $("#start-quiz-button").click(function() {
 });
 */
 
-startQuiz.addEventListener('click', function() {
-    setInterval(function(){ 
-        timer--;
-        timerEl.textContent = timer
+startQuiz.addEventListener('click', startCount);
+ 
+function startCount() {
+    //putting in the countdown
+
+    var timeInterval = setInterval(function(){ 
+        if (timer >= 1) {
+            timer--;
+            timerEl.textContent = timer
+        } if (timer === 0){
+            clearInterval(timerInterval);
+        }
     }, 1000);
     start.classList.add('hide')
     askQuestions();
     //stop the timer once it reaches 0
-});
-
-var askQuestions = function () {
-
-    var containerEl = document.querySelector('#container')
-
-    for (let i = 0; i < arrayOfQuestions.length; i++) {
-
-    // create question html div, and adding question text
-    var questionEl = document.createElement('div')
-    var question = arrayOfQuestions[i].question;
-    questionEl.textContent = question
-    containerEl.appendChild(questionEl)
-    
-
-    // create choices html div, and add choices text
-    var choicesEl = document.createElement('div')
-    var choices = arrayOfQuestions[i].choices;
-    var choicesOl = document.createElement('ol')
-    var choicesLi = document.createElement('li')
-    //var choicesLi2 = document.createElement('li')
-
-        choicesLi.textContent = choices[i]
-        //choicesLi2.textContent = choices[1]
-        choicesOl.appendChild(choicesLi)
-        //choicesOl.appendChild(choicesLi2)
-        choicesEl.appendChild(choicesOl)
-        //containerEl.appendChild(questionEl)
-        containerEl.appendChild(choicesEl)
-        
-       arrayOfQuestions[arrayOfQuestions.length-1]
-
-    }
-    //will show each question and its 4 optiosn
-    
-    //listen for the click for 'answerSelected', and populate if correct or wrong on the footer. if wrong, decrease timer by 10
-    // answerSelected.addEventListener('click', function() {
-    //     for (let i = 0; i < choicesArray.length; i++) {
-    //         if (answerSelected === false) //(write the proper code) {
-    //             //populate  in footer <p class "wrong">
-    //             wrongEl.textContent="Wrong!"
-    //             } else {
-    //                 //populate in footer <p class "correct"
-    //                 correctEl.textContent="Correct!"
-    //             }
-    //         }
-    //         $.get("footer.html", function(response){
-    //             $('footer').html(response);
-    //         });  
-
-    // var ulEl = createElement('ul');
-
-    // // loop over the array of choices
-    // for (let i = 0; i < choicesArray.length; i++) {
-    //     // create an html element like 'li'
-    //     var liEl = document.createElement('li');
-    //     // assign the choices array answer to the new li element using textContent
-    //     liEl.textContent = choicesArray[i];
-    //     // append liEl to ulEl
-    //     ulEl.appendChild(liEl);
-    // }
 };
- 
 
-var arrayOfQuestions = [ //add on footer if selector is right/wrong
-    {
-        // object/ question 1
+var questionsAndAnswers = [ //add on footer if selector is right/wrong
+    {   // object/ question 1
         question: 'Commonly used data types DO Not include:',
-        choices: [
-            'strings',
-            'booleans',
-            'alerts',
-            'numbers',
-        ],
+        answerOne: 'strings',
+        answerTwo: 'booleans',
+        answerThree: 'alerts',
+        answerFour: 'numbers',
         correctAnswer: 'alerts',
     },
-    {
-        // object/ question 2
+    {   // object/ question 2
         question: 'The condition in an if/else statement is enclosed with _______.',
-        choices: [
-            'quotes',
-            'curly brackets',
-            'parenthesis',
-            'square brackets',    
-        ],
+        answerOne: 'quotes',
+        answerTwo: 'curly brackets',
+        answerThree: 'parenthesis',
+        answerFour: 'square brackets',    
         correctAnswer: 'curly brackets',
     },
-    {
-        // object/ question 3
+    {   // object/ question 3
         question: 'Arrays in JavaScript can be used to store _______.',
-        choices: [
-            'numbers and strings',
-            'other arrays',
-            'booleans',
-            'all of the above',    
-        ],
+        answerOne: 'numbers and strings',
+        answerTwo: 'other arrays',
+        answerThree: 'booleans',
+        answerFour: 'all of the above',    
         correctAnswer: 'all of the above',
     }, 
-    {
-        // object/ question 4
+    {   // object/ question 4
         question: 'String values must be enclosed within _______ when being assigned to variables.',
-        choices: [
-            'commas',
-            'curly brackets',
-            'quotes',
-            'parenthesis',
-        ],
+        answerOne: 'commas',
+        answerTwo: 'curly brackets',
+        answerThree: 'quotes',
+        answerFour: 'parenthesis',
         correctAnswer: 'curly brackets',
     },
-    {
-        // object/ question 5
+    {    // object/ question 5
         question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
-        choices: [
-            'JavaScript',
-            'terminal/bash',
-            'for loops',
-            'console.log',
-        ],
+        answerOne: 'JavaScript',
+        answerTwo: 'terminal/bash',
+        answerThree: 'for loops',
+        answerFour: 'console.log',
         correctAnswer: 'console.log',
     }
 ];
 
+
+//appending the questions and answer elements
+var askQuestions = function () {
+
+
+    for (let i = 0; i < questionsAndAnswers.length; i++) {
+        document.getElementById(questionsAndAnswers[i].questions + "<br/>");
+        for (let j = 0; j < questionsAndAnswers[i].choices.length; j++) {
+        document.getElementById(questionsAndAnswers[i].choices[j] + "<br/>");
+    
+// create question html div, and adding question text 
+    quizQuestion = document.createElement('h2');
+    quizQuestion.className = "quiz-question";
+    quizQuestion = questionsAndAnswers[i].question;
+    quizQuestion.textContent = question;
+    quizBox.appendChild(quizQuestion);
+
+    quizAnswerBox = document.createElement('ul');
+
+    quizAnswerOne = document.createElement('li');
+    quizAnswerTwo = document.createElement('li');
+    quizAnswerThree = document.createElement('li');
+    quizAnswerFour = document.createElement('li');
+
+    quizAnswerOne.className = "quiz-answer";
+    quizAnswerTwo.className = "quiz-answer";
+    quizAnswerThree.className = "quiz-answer";
+    quizAnswerFour.className = "quiz-answer";
+
+    quizBox.appendChild(quizAnswerBox);
+    quizBox.appendChild(quizAnswerOne);
+    quizBox.appendChild(quizAnswerTwo);
+    quizBox.appendChild(quizAnswerThree);
+    quizBox.appendChild(quizAnswerFour);
+
+    quizAnswerOne.addEventListener('click', answerCheck);
+    quizAnswerTwo.addEventListener('click', answerCheck);
+    quizAnswerThree.addEventListener('click', answerCheck);
+    quizAnswerFour.addEventListener('click', answerCheck);
+
+   
+//set attribute so other question and choice sets are hidden (data state), and visible after the previous click
+
+viewHighScores.addEventListener('click', function() {
+    start.classList.add('hide')
+    askQuestions();
+    //stop the timer once it reaches 0
+});
 //Create a footer section with thin line immediately after the last question
 //build capability to capture in the footer if the selected response is 'Correct!' or 'Wrong!'
 //and when answer is selected, change button color to brighter color
 
 var yourFinalScoreEl = document.querySelector('#yourFinalScore');
-//dynamically input 'Your final score is XX.', with XX being user's current score
-document.querySelector("#yourFinalScore")
-var finalScoreEl = document.createElement('initials');
+yourFinalScoreEl.textContent = finalScore
+yourFinalScore.appendChild('yourFinalScoreEl')
+
+var finalScoreEl = document.createElement('p');
+
+
+var initials = document.querySelector('.enter-initials');
+finalScoreEl.textcontent= initials
+yourFinalScore.appendChild('initials')
 //after the text Enter Initials:, build a <textarea> for user to input initials to submit initialEl
-document.textContent.
-onClick()
-
+//document.textContent.
+//onClick()
 var viewHighScoresEl = document.querySelector('#viewHighScores');
-//include 'Initials-Value of high score
-//two buttons: Go back && Clear high scores
+viewHighScoresEl.textContent=""
 
-//when StartQuiz button is clicked, timer starts at 75
-//then it subtracts by 10 if selected answer is wrong
-//timer stops when all questions are answered
+//checking local storage for current high schore
+var highScore = localStorage.getItem("highScore")
+if (highScore) {
+    highScore = 0
+} else {
+    highScore= parseInt(highScore);
+};
